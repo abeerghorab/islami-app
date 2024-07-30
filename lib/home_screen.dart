4a629b27bb9901df6e25713dd876768842b36ef2
abeer <beroghorab068@gmail.com>
2024-07-30 14:13:28 +0300

    tabs

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,126 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:islami/My_theme.dart';
-import 'package:islami/sura_name.dart';
 
-class HomeScreen extends StatelessWidget {
+import 'package:islami/home_tab/hadeth_tab.dart';
+import 'package:islami/home_tab/quran_tab.dart';
+import 'package:islami/home_tab/radio_tab.dart';
+import 'package:islami/home_tab/tasbeh_tab.dart';
+
+class HomeScreen extends StatefulWidget {
   HomeScreen({super.key});
   static const String routeName = "Home-Screen";
-  List<String> names = [
-    "الفاتحة",
-    "البقرة",
-    "آل عمران",
-    "النساء",
-    "المائدة",
-    "الأنعام",
-    "الأعراف",
-    "الأنفال",
-    "التوبة",
-    "يونس",
-    "هود",
-    "يوسف",
-    "الرعد",
-    "إبراهيم",
-    "الحجر",
-    "النحل",
-    "الإسراء",
-    "الكهف",
-    "مريم",
-    "طه",
-    "الأنبياء",
-    "الحج",
-    "المؤمنون",
-    "النور",
-    "الفرقان",
-    "الشعراء",
-    "النمل",
-    "القصص",
-    "العنكبوت",
-    "الروم",
-    "لقمان",
-    "السجدة",
-    "الأحزاب",
-    "سبأ",
-    "فاطر",
-    "يس",
-    "الصافات",
-    "ص",
-    "الزمر",
-    "غافر",
-    "فصلت",
-    "الشورى",
-    "الزخرف",
-    "الدخان",
-    "الجاثية",
-    "الأحقاف",
-    "محمد",
-    "الفتح",
-    "الحجرات",
-    "ق",
-    "الذاريات",
-    "الطور",
-    "النجم",
-    "القمر",
-    "الرحمن",
-    "الواقعة",
-    "الحديد",
-    "المجادلة",
-    "الحشر",
-    "الممتحنة",
-    "الصف",
-    "الجمعة",
-    "المنافقون",
-    "التغابن",
-    "الطلاق",
-    "التحريم",
-    "الملك",
-    "القلم",
-    "الحاقة",
-    "المعارج",
-    "نوح",
-    "الجن",
-    "المزمل",
-    "المدثر",
-    "القيامة",
-    "الإنسان",
-    "المرسلات",
-    "النبأ",
-    "النازعات",
-    "عبس",
-    "التكوير",
-    "الإنفطار",
-    "المطففين",
-    "الإنشقاق",
-    "البروج",
-    "الطارق",
-    "الأعلى",
-    "الغاشية",
-    "الفجر",
-    "البلد",
-    "الشمس",
-    "الليل",
-    "الضحى",
-    "الشرح",
-    "التين",
-    "العلق",
-    "القدر",
-    "البينة",
-    "الزلزلة",
-    "العاديات",
-    "القارعة",
-    "التكاثر",
-    "العصر",
-    "الهمزة",
-    "الفيل",
-    "قريش",
-    "الماعون",
-    "الكوثر",
-    "الكافرون",
-    "النصر",
-    "المسد",
-    "الإخلاص",
-    "الفلق",
-    "الناس",
-  ];
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -143,6 +38,11 @@ class HomeScreen extends StatelessWidget {
             data: Theme.of(context)
                 .copyWith(canvasColor: Theme.of(context).primaryColor),
             child: BottomNavigationBar(
+              currentIndex: selectedIndex,
+              onTap: (index) {
+                selectedIndex = index;
+                setState(() {});
+              },
               items: [
                 BottomNavigationBarItem(
                     icon: ImageIcon(
@@ -173,45 +73,11 @@ class HomeScreen extends StatelessWidget {
               ],
             ),
           ),
-          body: Column(
-            children: [
-              Center(
-                child: Image.asset(
-                  "Assets/quran3.jpg",
-                  width: 150,
-                  height: 200,
-                ),
-              ),
-              Divider(
-                color: MyThemeData.primartLight,
-                thickness: 2,
-              ),
-              Text(
-                "sura Name",
-                style: Theme.of(context).textTheme.subtitle1,
-              ),
-              Divider(
-                color: MyThemeData.primartLight,
-                thickness: 2,
-              ),
-              Expanded(
-                child: ListView.separated(
-                  separatorBuilder: (context, index) {
-                    return Divider(
-                      color: MyThemeData.primartLight,
-                      thickness: 2,
-                    );
-                  },
-                  itemBuilder: (context, index) {
-                    return SuraName(name: names[index]);
-                  },
-                  itemCount: names.length,
-                ),
-              )
-            ],
-          ),
+          body: tabs[selectedIndex],
         ),
       ],
     );
   }
+
+  List<Widget> tabs = [QuranTab(), HadethTab(), TasbehTab(), RadioTab()];
 }
