2c22487e6be1e493bbc50de3bcaa2dbc8820aef7
abeer <beroghorab068@gmail.com>
2024-07-30 16:18:42 +0300

    load-quran-file

--- lib/Quran/sura_details_Screen.dart
+++ lib/Quran/sura_details_Screen.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:islami/My_theme.dart';
 
-class SuraDetailsScreen extends StatelessWidget {
+class SuraDetailsScreen extends StatefulWidget {
   static const String routeName = "sura-details-screen";
+
+  @override
+  State<SuraDetailsScreen> createState() => _SuraDetailsScreenState();
+}
+
+class _SuraDetailsScreenState extends State<SuraDetailsScreen> {
+  List<String> verses = [];
 
   @override
   Widget build(BuildContext context) {
     var args = ModalRoute.of(context)?.settings.arguments as SuraDetailsArgs;
+    loadQuranFile(args.index);
     return Stack(
       children: [
         Image.asset(
@@ -22,9 +32,34 @@ class SuraDetailsScreen extends StatelessWidget {
               style: Theme.of(context).textTheme.headline1,
             ),
           ),
+          body: Container(
+            decoration: BoxDecoration(
+                color: MyThemeData.whiteColor,
+                borderRadius: BorderRadius.circular(24)),
+            margin: EdgeInsets.symmetric(horizontal: 24, vertical: 40),
+            child: ListView.separated(
+                itemBuilder: (context, index) {
+                  return Text(verses[index]);
+                },
+                separatorBuilder: (context, index) {
+                  return Divider(
+                    color: Theme.of(context).primaryColor,
+                    thickness: 2,
+                  );
+                },
+                itemCount: verses.length),
+          ),
         ),
       ],
     );
+  }
+
+  void loadQuranFile(int index) async {
+    String fileContent =
+        await rootBundle.loadString("Assets/files/${index + 1}.txt");
+    List<String> lines = fileContent.split("\n");
+    verses = lines;
+    setState(() {});
   }
 }
 
