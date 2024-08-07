f5d40c367e0af2339bc077694b9e91d547d79764
abeer <beroghorab068@gmail.com>
2024-08-07 12:11:09 +0300

    get selected-unselected-Item

--- lib/settings/language_bottom_sheet.dart
+++ lib/settings/language_bottom_sheet.dart
@@ -3,7 +3,12 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:islami/provider/app_config_provider.dart';
 import 'package:provider/provider.dart';
 
-class LanguageBottomSheet extends StatelessWidget {
+class LanguageBottomSheet extends StatefulWidget {
+  @override
+  State<LanguageBottomSheet> createState() => _LanguageBottomSheetState();
+}
+
+class _LanguageBottomSheetState extends State<LanguageBottomSheet> {
   @override
   Widget build(BuildContext context) {
     var provider = Provider.of<AppConfigProvider>(context);
@@ -13,24 +18,13 @@ class LanguageBottomSheet extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           InkWell(
-            onTap: () {
-              provider.changeLanguage("en");
-            },
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  AppLocalizations.of(context)!.english,
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-                Icon(
-                  Icons.check,
-                  size: 30,
-                  color: Theme.of(context).primaryColor,
-                )
-              ],
-            ),
-          ),
+              onTap: () {
+                provider.changeLanguage("en");
+              },
+              child: provider.appLanguage == "en"
+                  ? getSelectedItemWidget(AppLocalizations.of(context)!.english)
+                  : getUnSelectedItemWidget(
+                      AppLocalizations.of(context)!.english)),
           SizedBox(
             height: 15,
           ),
@@ -38,13 +32,36 @@ class LanguageBottomSheet extends StatelessWidget {
             onTap: () {
               provider.changeLanguage("ar");
             },
-            child: Text(
-              AppLocalizations.of(context)!.arabic,
-              style: Theme.of(context).textTheme.subtitle1,
-            ),
+            child: provider.appLanguage == "ar"
+                ? getSelectedItemWidget(AppLocalizations.of(context)!.arabic)
+                : getUnSelectedItemWidget(AppLocalizations.of(context)!.arabic),
           ),
         ],
       ),
+    );
+  }
+
+  Widget getSelectedItemWidget(String text) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          text,
+          style: Theme.of(context).textTheme.headline4,
+        ),
+        Icon(
+          Icons.check,
+          size: 30,
+          color: Theme.of(context).primaryColor,
+        )
+      ],
+    );
+  }
+
+  Widget getUnSelectedItemWidget(String text) {
+    return Text(
+      text,
+      style: Theme.of(context).textTheme.subtitle1,
     );
   }
 }
--- lib/settings/settings.dart
+++ lib/settings/settings.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:islami/My_theme.dart';
+import 'package:islami/provider/app_config_provider.dart';
 import 'package:islami/settings/language_bottom_sheet.dart';
+import 'package:provider/provider.dart';
 
 class SettingTab extends StatefulWidget {
   const SettingTab({super.key});
@@ -13,6 +15,7 @@ class SettingTab extends StatefulWidget {
 class _SettingTabState extends State<SettingTab> {
   @override
   Widget build(BuildContext context) {
+    var provider = Provider.of<AppConfigProvider>(context);
     return Container(
       margin: EdgeInsets.all(18),
       child: Column(
@@ -39,7 +42,47 @@ class _SettingTabState extends State<SettingTab> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
-                    AppLocalizations.of(context)!.english,
+                    provider.appLanguage == "en"
+                        ? AppLocalizations.of(context)!.english
+                        : AppLocalizations.of(context)!.arabic,
+                    style: Theme.of(context).textTheme.headline3,
+                  ),
+                  Icon(
+                    Icons.arrow_drop_down_outlined,
+                    size: 30,
+                    color: MyThemeData.whiteColor,
+                  )
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 18,
+          ),
+          Text(
+            AppLocalizations.of(context)!.language,
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
+          SizedBox(
+            height: 18,
+          ),
+          Container(
+            padding: EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(24),
+              color: Theme.of(context).primaryColor,
+            ),
+            child: InkWell(
+              onTap: () {
+                showLanguageBottomSheet();
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    provider.appLanguage == "en"
+                        ? AppLocalizations.of(context)!.english
+                        : AppLocalizations.of(context)!.arabic,
                     style: Theme.of(context).textTheme.headline3,
                   ),
                   Icon(
